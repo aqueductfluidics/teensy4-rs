4d879b20c624eaf5683b44bc2e8868c683b73d70
djstrickland <96876452+dstric-aqueduct@users.noreply.github.com>
2023-01-05 08:07:21 -0500

    Update usb.rs

--- src/usb.rs
+++ src/usb.rs
@@ -344,14 +344,11 @@ struct Logger {
     /// A collection of targets that we are expected
     /// to filter. If this is empty, we allow everything
     filters: Filters,
-    /// Write Data
-    write_data: bool,
 }
 
 static mut LOGGER: Logger = Logger {
     enabled: false,
     filters: Filters::empty(),
-    write_data: false,
 };
 
 impl ::log::Log for Logger {
@@ -365,21 +362,15 @@ impl ::log::Log for Logger {
     fn log(&self, record: &::log::Record) {
         if self.enabled(record.metadata()) {
             use core::fmt::Write;
-            let result = if self.write_data {
-                cortex_m::interrupt::free(|_| {
-                    writeln!(unsafe { Writer::new() }, "[DATA]: {}", record.args())
-                })
-            } else {
-                cortex_m::interrupt::free(|_| {
-                    writeln!(
-                        unsafe { Writer::new() },
-                        "[{} {}]: {}",
-                        record.level(),
-                        record.target(),
-                        record.args()
-                    )
-                })
-            };
+            let result = cortex_m::interrupt::free(|_| {
+                writeln!(
+                    unsafe { Writer::new() },
+                    "[{} {}]: {}",
+                    record.level(),
+                    record.target(),
+                    record.args()
+                )
+            });
             assert!(result.is_ok());
         }
     }
@@ -389,17 +380,11 @@ impl ::log::Log for Logger {
     }
 }
 
-pub fn write_data<F>(f: F)
-where
-    F: FnOnce() -> (),
-{
-    unsafe {
-        LOGGER.write_data = true;
-    }
-    f();
-    unsafe {
-        LOGGER.write_data = false;
-    }
+pub fn write_data<B: AsRef<[u8]>>(b: B) {
+    let mut w = unsafe { Writer::new() };
+    w.write("(D): ").ok();
+    w.write(b).ok();
+    w.write("\r\n").ok();
 }
 
 /// A type that can send data to a USB serial host
